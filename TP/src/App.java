ac33750c12b5bc201344697c8d54202ef9d851d0
Elabedamna <elabed.aminaaa@gmail.com>
2023-05-13 16:59:40 +0100

    amina/name+icon

--- TP/src/App.java
+++ TP/src/App.java
@@ -3,6 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -11,10 +12,15 @@ import java.io.IOException;
 
 public class App extends Application {
     @Override
+    
     public void start(Stage stage) throws IOException {
+            // Set the icon to your custom icon
+        
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("interface/Preference.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
-        stage.setTitle("Authentification");
+        stage.setTitle("Planify");
+        Image icon = new Image(getClass().getResourceAsStream("/interface/Calendrier logo 3.png"));
+        stage.getIcons().add(icon); 
         stage.setScene(scene);
         stage.show();
     }
